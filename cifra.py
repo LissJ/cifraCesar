518ba882e181720c527dcf4580cb69217265a57c
Liss <133662367+LissJ@users.noreply.github.com>
2025-03-30 17:16:48 -0300

    terminando comentarios

--- cifra.py
+++ cifra.py
@@ -1,22 +1,42 @@
+# importando a função 'sys', que permite a manipulação do sistema
 import sys
+
+# importando a função 'unidecode', que permite remover acentos das letras
+        # como por exemplo: é = e
 from unidecode import unidecode
 
+# imprimindo linhas e uma mensagem que marca o início do código da cifra de césar
 print('--------------------------------------------'
-        '\nIniciando Cifra de César'
-        '\n--------------------------------------------')
+      '\nIniciando Cifra de César'
+      '\n--------------------------------------------')
+
+# atribuindo um valor para a variável 'mensagem', que o próprio usuário insere
+# a função 'lower()' serve para que todos os caracteres se padronizem, fazendo com que sejam apenas letras minúsculas
 mensagem = input('Insira uma frase: ').lower()
 
+# crio uma condição de erro
 try:
+
+    # atribuo o valor inserido pelo usuário para a variável 'deslocamento', já que ela será a responsável por delimitar quantos caracteres serão pulados
+            # e o valor deve ser lido como número inteiro
     deslocamento = int(input('Deslocamento: '))
 
+    # se o valor atribuído ao deslocamento for maior do que 25
     if deslocamento > 25:
+        # uma mensagem de erro será impressa, solicitando que o valor inserido seja entre 0 e 25, e que o programa será encerrado
         print('--------------------------------------------'
-                '\nPor favor, insira um número entre 0 e 25.'
-                '\n--------------------------------------------'
-                '\nEncerrando programa.')
+              '\nErro. Por favor, insira um número entre 0 e 25.'
+              '\n--------------------------------------------'
+              '\nEncerrando programa.')
+        # saindo do sistema, ou seja, encerrando-o
         sys.exit()
+
+# se o usuário inserir algo que não seja um número
 except ValueError:
-    print('Por favor, insira um número.')
+
+    # uma mensagem de erro será impressa
+    print('Por favor, insira um número. Encerrando programa.')
+    # saindo do sistema, ou seja, encerrando-o
     sys.exit()
     
 modo = input('Positivo ou negativo? (p/n) ').strip().lower()
